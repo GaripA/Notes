c45be87d9de4a4558a1de3367af3b3b95082649d
GaripA <garip.akkaya9@gmail.com>
2024-01-24 17:55:17 +0100

    commit 35

--- Models/Course.cs
+++ Models/Course.cs
@@ -2,8 +2,8 @@
 {
     public class Course
     {
-        public int CourseId { get; set; } // Ajout de cette propriété
+        
         public string CourseName { get; set; }
-        public int ResponsibleStudentId { get; set; }
+        
     }
 }
