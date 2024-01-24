a354302b8fa6e4064c9ca3bfd8728a79e5475822
GaripA <garip.akkaya9@gmail.com>
2024-01-24 16:09:22 +0100

    commit 29

--- Models/Evaluation.cs
+++
@@ -1,9 +0,0 @@
-﻿// Evaluation.cs
-namespace Notes.Models
-{
-    public class Evaluation
-    {
-        public string CurseName { get; set; }
-        public int Score { get; set; } // ou string pour l'appréciation codée
-    }
-}
--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Notes.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Notes.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+831ee7700ed8122c6c06de82cc5235a98a92ccca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d9a12cf424f3853ecd0143948caea546379f91fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
